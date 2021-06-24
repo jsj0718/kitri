4467e29a6e58a273345d0dcc5ebca5eeb1cadc65
jsj0718 <jeongsejin718@gmail.com>
2021-06-24 16:02:48 +0900

    KITRI Collection Framework 수업 내용

---
+++ JavaCh11Prj/src/arraylist/TestClass.java
@@ -0,0 +1,35 @@
+package arraylist;
+
+public class TestClass {
+	String str;
+	int var;
+	boolean flag;
+	float fvar;
+	
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public int getVar() {
+		return var;
+	}
+	public void setVar(int var) {
+		this.var = var;
+	}
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	public float getFvar() {
+		return fvar;
+	}
+	public void setFvar(float fvar) {
+		this.fvar = fvar;
+	}
+	
+	
+}
